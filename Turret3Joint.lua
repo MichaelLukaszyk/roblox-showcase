f5031bb1cd2df28d500173db938ef39a81b19fee
Michael Lukaszyk <ml168@illinois.edu>
2026-07-22 14:32:37 -0500

    Updated

--- Turret3Joint.lua
+++ Turret3Joint.lua
@@ -7,12 +7,15 @@ local ReticleManager = require(ReplicatedStorage.Modules.ReticleManager)
 local MasterInput = require(ReplicatedStorage.Modules.MasterInput)
 local Mouse = require(ReplicatedStorage.Modules.Mouse)
 local Math = require(ReplicatedStorage.Modules.Math)
-local WeldEvent = ReplicatedStorage.Events.WeldEvent
 local Settings = require(script:WaitForChild("Settings").Value)
 
 local Player = Players.LocalPlayer
 local Blacklist = {Player.Character, Settings["Vehicle"] or Settings["Turret"]}
+local Params = RaycastParams.new()
+Params.FilterType = Enum.RaycastFilterType.Exclude
+Params.FilterDescendantsInstances = Blacklist
 
+local WeldEvent = Settings["Turret"]:WaitForChild("WeldEvent")
 local Attachments = Settings["Turret"]:WaitForChild("Attachments")
 local BaseAttach = Attachments:WaitForChild("Base")
 local BaseYawAttach = Attachments:WaitForChild("BaseYaw")
@@ -32,41 +35,51 @@ local Rotation = {
 	[2] = math.deg(BaseYawY),
 	[3] = math.deg(YawY)
 }
-
+local OldBaseRot = nil
 local MouseInfo = {
 	["UseTouchCenter"] = true
 }
 
+while FirstPersonTool.State.FakeModel do
+	task.wait()
+end
 local Model = Settings["Turret"]:FindFirstChild("Model")
 local Base = Model and Model:FindFirstChild("Base")
 local NormalCamera = Settings["NormalCamera"] or (Base and Base:FindFirstChild("NormalCamera"))
 local MobileCamera = Settings["MobileCamera"] or (Base and Base:FindFirstChild("MobileCamera"))
 if MobileCamera and Player.PlayerGui:FindFirstChild("TouchGui") then
-	FirstPersonTool["Subject_3rd"].Value = MobileCamera
+	FirstPersonTool.SetSubject_3rd(MobileCamera)
 elseif NormalCamera then
-	FirstPersonTool["Subject_3rd"].Value = NormalCamera
+	FirstPersonTool.SetSubject_3rd(NormalCamera)
 end
 
 local Pitch = Model and Model:FindFirstChild("Pitch")
 local FirstPerson = Pitch and Pitch:FindFirstChild("FirstPerson")
 if FirstPerson then
 	Settings["SightsPart"] = Pitch:FindFirstChild("Sights")
-	FirstPersonTool["Subject_FPS"].Value = FirstPerson
-	FirstPersonTool:ConnectAimDown(Settings)
+	FirstPersonTool.SetSubject_FPS(FirstPerson)
+	FirstPersonTool.AimDown.Connect(Settings)
 end
 
 if Barrel then
-	ReticleManager:Add(Barrel, Blacklist)
+	ReticleManager:Add(Barrel, Params)
 end
+
+BaseYawWeld:SetAttribute("ClientWeld", true)
+YawWeld:SetAttribute("ClientWeld", true)
+PitchWeld:SetAttribute("ClientWeld", true)
 
 local Seat = Settings["Seat"]
 if Seat then
 	local PlayerValue = Seat:WaitForChild("PlayerValue")
 	PlayerValue:GetPropertyChangedSignal("Value"):Connect(function()
 		if not PlayerValue.Value then
-			FirstPersonTool["Subject_3rd"].Value = nil
-			FirstPersonTool["Subject_FPS"].Value = nil
+			FirstPersonTool.SetSubject_3rd(nil)
+			FirstPersonTool.SetSubject_FPS(nil)
 			ReticleManager:Remove(Barrel)
+			BaseYawWeld:SetAttribute("ClientWeld", nil)
+			YawWeld:SetAttribute("ClientWeld", nil)
+			PitchWeld:SetAttribute("ClientWeld", nil)
 			script:Destroy()
 		end
 	end)
@@ -78,11 +91,12 @@ local b = Offset.Y
 local c = math.sqrt(a ^ 2 + b ^ 2)
 local phi = math.atan2(b, a)
 
-RunService:BindToRenderStep("Turret", Enum.RenderPriority.Camera.Value + 1, function(Delta)
+--//Must be after simulation since it grabs part.CFrame, works best with PreRender
+RunService.PreRender:Connect(function(Delta)
 	local OldPitch = Rotation[1]
 	local OldYaw = Rotation[2] + Rotation[3]
 	local OldBaseYaw = Rotation[2]
-	if FirstPersonTool["FirstPerson"] then
+	if Player:GetAttribute("FirstPerson") then
 		--//Use input delta
 		local RotDelta = MasterInput:GetRotation(Delta)
 		local Max = Settings["Speed"] * Delta
@@ -98,7 +112,7 @@ RunService:BindToRenderStep("Turret", Enum.RenderPriority.Camera.Value + 1, func
 			return math.cos(x)*(x2 - c*math.cos(x + phi)) + math.sin(x)*(y2 - c*math.sin(x + phi)) - math.sqrt((x2 - c*math.cos(x + phi))^2 + (y2 - c*math.sin(x + phi))^2)
 		end
 		local Pitch = math.deg(Math:NewtonsMethod(f, 0.001, 0, 0.001))
-		
+
 		local YawCenter = BaseAttach.CFrame.Rotation + YawAttach.Position
 		local YawOffset = YawCenter:ToObjectSpace(CFrame.new(Position))
 		local Yaw = math.deg(math.atan2(YawOffset.X, -YawOffset.Z))
@@ -131,6 +145,20 @@ RunService:BindToRenderStep("Turret", Enum.RenderPriority.Camera.Value + 1, func
 		Rotation[3] = -Settings["YawGap"]
 	end
 	
+	--//Decouple rotation
+	if Player:GetAttribute("DecoupleTurretRotation") then
+		local PitchX, YawY, _ = BaseAttach.CFrame:ToOrientation()
+		local BaseRot = {
+			[1] = math.deg(-PitchX),
+			[2] = math.deg(YawY)
+		}
+		if OldBaseRot then
+			Rotation[1] += BaseRot[1] - OldBaseRot[1]
+			Rotation[2] += BaseRot[2] - OldBaseRot[2]
+		end
+		OldBaseRot = BaseRot
+	end
+	
 	--//Bound values
 	Rotation[1] = math.clamp(
 		Rotation[1],
@@ -148,12 +176,11 @@ RunService:BindToRenderStep("Turret", Enum.RenderPriority.Camera.Value + 1, func
 	end
 	
 	--//Welds
-	BaseYawWeld.C0 = CFrame.fromOrientation(0, math.rad(Rotation[2]), 0)
-	YawWeld.C0 = CFrame.fromOrientation(0, math.rad(Rotation[3]), 0)
-	PitchWeld.C0 = CFrame.fromOrientation(math.rad(-Rotation[1]), 0, 0)
-	WeldEvent:FireServer(
-		{["Weld"] = BaseYawWeld, ["C0"] = BaseYawWeld.C0},
-		{["Weld"] = YawWeld, ["C0"] = YawWeld.C0},
-		{["Weld"] = PitchWeld, ["C0"] = PitchWeld.C0}
-	)
+	local BaseYaw = math.rad(Rotation[2])
+	local Yaw = math.rad(Rotation[3])
+	local Pitch = math.rad(-Rotation[1])
+	BaseYawWeld.C0 = CFrame.fromOrientation(0, BaseYaw, 0)
+	YawWeld.C0 = CFrame.fromOrientation(0, Yaw, 0)
+	PitchWeld.C0 = CFrame.fromOrientation(Pitch, 0, 0)
+	WeldEvent:FireServer(BaseYaw, Yaw, Pitch)
 end)
